73088033f087d1b21d5bbee17d5341d46538e700
WebServices <webservices@iMac-de-QuaddroAdm.local>
2017-08-11 20:52:18 -0300

    Implementando um servidorzinho na unha

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Desenvolvido com NOdeJS</title>
+  </head>
+  <body>
+    <h1>Servidor na Unha</h1>
+    <h2>Andriu Coelho</h2>
+  </body>
+</html>`;
